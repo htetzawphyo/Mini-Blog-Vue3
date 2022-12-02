a1c61122615edd6f8d437257e7f8802fa6e9c414
htetzawphyo <htetzawphyo.hpn@gmail.com>
2022-12-02 15:18:35 +0630

    delete post query firebase

--- src/views/PostDetail.vue
+++ src/views/PostDetail.vue
@@ -14,6 +14,9 @@
                 <router-link :to="{name:'tag', params:{tag:tag}}"><span class="text-light text-decoration-none">{{tag}}</span></router-link>
               </span>
             </div>
+            <div class="card-footer">
+              <button class="btn btn-danger" @click="postDelete">Delete</button>
+            </div>
           </div>
         </div>
         <div v-else-if="!error && !post">
@@ -32,16 +35,28 @@
 import Spinner from '../components/Spinner'
 import getPost from '@/composables/getPost';
 import { useRoute } from 'vue-router';
+import { useRouter } from 'vue-router';
+import { doc, deleteDoc } from "@firebase/firestore/lite";
+import { db } from "@/firebase/config";
 
 export default {
   components: { Spinner },
     props: ['id'],
     setup(props) {
         let route = useRoute();
+        let router = useRouter();
         let {post, error, load} = getPost(route.params.id); // composables folder -> getPost() // route.params.id === props.id
         load();
+        
+        let postDelete = async() => {
+          let id = props.id;
+          // let docRef = doc(db, "posts", id);
+          await deleteDoc(doc(db, "posts", id));
 
-        return {post, error}
+          router.push('/');
+        }
+
+        return {post, error, postDelete}
     }
 }
 </script>
